772b116fb0677c277fafc85325a6ff93c11f8565
Abhi@@@2607 <jainabhishekgwl904@gmail.com>
2021-03-16 17:32:33 +0530

    initial set up

---
+++ admin/language/en-gb/extension/report/product_purchased.php
@@ -0,0 +1,57 @@
+<?php
+// Heading
+$_['heading_title']     = 'Products Purchased Report';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_edit']         = 'Edit Products Purchased Report';
+$_['text_success']      = 'Success: You have modified products purchased report!';
+$_['text_filter']       = 'Filter';
+$_['text_all_status']   = 'All Statuses';
+
+// Column
+$_['column_date_start']  = 'Date Start';
+$_['column_date_end']    = 'Date End';
+$_['column_name']        = 'Product Name';
+$_['column_model']       = 'Model';
+$_['column_quantity']    = 'Quantity';
+$_['column_total']       = 'Total';
+$_['column_year']        = 'Year';
+$_['column_month']       = 'Month';
+$_['column_invoice_date']= 'Inv. Date';
+$_['column_client_name'] = 'Client Name';
+$_['column_client_business'] = 'Client Business Name';
+$_['column_gst']         = 'Receipient GSTN';
+$_['column_amt_transfer_curr'] = 'Received Amt (Trans. Curr.)';
+$_['column_currency']    = 'Currency';
+$_['column_exch_rate']   = 'Exch.Rate';
+$_['column_receving_amount'] = 'Receving Amt (LCY)';
+
+$_['column_igst']        = 'IGST';
+$_['column_cgst']        = 'CGST';
+$_['column_sgst']        = 'SGST';
+$_['column_tds']         = 'TDS %';
+$_['column_tds_deduction']   = 'TDS Deduction';
+$_['column_total_amount']    = 'Total Amt (LCY)';
+$_['column_payment_date']    = 'Payment Date';
+$_['column_cutoff']          = 'Cut Off/Bank Comm.(usd)';
+$_['column_base_usd']           = 'Base USD';
+$_['column_based_exchange_rate']= 'Based Exch. Rate';
+$_['column_based_amount']       = 'Based Amt (LCY)';
+$_['column_source_from']        = 'Source From';
+$_['column_bank']               = 'Bank';
+$_['column_acc_name']           = 'Acc Name';
+$_['column_transaction']        = 'Transaction';
+$_['column_part_of_income']     = 'Part of Income';
+
+
+
+// Entry
+$_['entry_date_start']  = 'Date Start';
+$_['entry_date_end']    = 'Date End';
+$_['entry_status']      = 'Order Status';
+$_['entry_status']      = 'Status';
+$_['entry_sort_order']  = 'Sort Order';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify products purchased report!';
